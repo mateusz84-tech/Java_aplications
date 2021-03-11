07a227f60a5cc8dfcbae2e3057a63d5e516e444c
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-11 18:05:52 +0100

    Class Student

---
+++ src/main/java/simple_processing_App/Student.java
@@ -0,0 +1,48 @@
+package simple_processing_App;
+
+public class Student {
+
+    private final String name;
+    private int grade;
+
+    public Student(String name, int grade){
+        if(name == null){
+            throw new IllegalArgumentException("Imię nie może być nulem.");
+        }
+        if(name.isEmpty() || name.isBlank()){
+            throw new IllegalArgumentException("Imię nie może być puste.");
+        }
+        if(grade < 0){
+            throw new IllegalArgumentException("Wynik musi być większy od zera.");
+        }
+        if(grade > 100){
+            throw new IllegalArgumentException("Maksymalny wynik 100.");
+        }
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        if(grade < 0){
+            throw new IllegalArgumentException("Wynik nie może być mniejszy od zera.");
+        }
+        if(grade > 100){
+            throw new IllegalArgumentException("Maksymalny wynik 100.");
+        }
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s, %s: %d%n",
+                "Imię",getName(),"wynik",getGrade());
+    }
+}
