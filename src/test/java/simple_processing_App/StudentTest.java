9b3a0a6100bc8310b48838f3372c1ab14b240e31
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-12 19:31:12 +0100

    Create test to new Student object.

--- src/test/java/simple_processing_App/StudentTest.java
+++ src/test/java/simple_processing_App/StudentTest.java
@@ -11,5 +11,17 @@ public class StudentTest {
         Student student = new Student("Name");
         assertEquals("Name", student.getName());
     }
+    @Test(expected = IllegalArgumentException.class)
+    public void createStudentWithNullValue(){
+        Student student = new Student(null);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void createStudentWithEmptyName(){
+        Student student = new Student("");
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void createStudentWithWhiteSpacesName(){
+        Student student = new Student("   ");
+    }
 
 }
