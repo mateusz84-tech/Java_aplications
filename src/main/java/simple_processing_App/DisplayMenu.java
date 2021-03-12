eea6b7868a83dcef2f4d5893f44fd5d1eb6e5ae5
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-12 20:06:45 +0100

    Create menu class to display option.

---
+++ src/main/java/simple_processing_App/DisplayMenu.java
@@ -0,0 +1,17 @@
+package simple_processing_App;
+
+public final class DisplayMenu {
+
+    public static void display(){
+        System.out.printf("%10s%n","===== Menu =====");
+        System.out.printf("%s%n","1. Dodaj studenta.");
+        System.out.printf("%s%n","2. Przypisz ocenę.");
+        System.out.printf("%s%n","3. Wyszukaj studenta.");
+        System.out.printf("%s%n","4. Pokaż wszystkich studentów.");
+        System.out.printf("%s%n","5. Pokaż średnią z egzaminu.");
+        System.out.printf("%s%n","6. Pokaż studenta z najwyższą oceną");
+        System.out.printf("%s%n","7. Pokaż studenta z najniższą oceną.");
+        System.out.printf("%s%n","8. Graficzne przedstawienie wyników.");
+        System.out.printf("%s%n","9. Zakończ program.");
+    }
+}
