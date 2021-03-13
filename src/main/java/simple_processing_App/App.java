fa997b941a2eaa7c38d263d45f2081d65819e04e
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-13 18:25:47 +0100

    Created Scanner object, and new list to keep Students. Added in menu display and selection options.

--- src/main/java/simple_processing_App/App.java
+++ src/main/java/simple_processing_App/App.java
@@ -1,9 +1,33 @@
 package simple_processing_App;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
 
+        Scanner inputChose = new Scanner(System.in);
+        List<Student> list = new ArrayList<>();
+
         DisplayMenu.display();
+
+        System.out.printf("%s: ","Wybierz");
+        while(!inputChose.hasNextInt()){
+            inputChose.next();
+            System.out.println("Niepoprawny wybór. Spróbuj jeszcze raz: ");
+        }
+        int chose = inputChose.nextInt();
+
+        switch (chose){
+            case 1:{
+                FunctionManager.addNewStudent(list);
+            }break;
+
+            default:
+                System.out.println("Brak takiej opcji.");
+        }
+
     }
 }
