be1877a9ff29c0086b953e3f9e7c570bc944e274
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-13 20:38:44 +0100

    Added new function, find student by Name.

--- src/main/java/simple_processing_App/FunctionManager.java
+++ src/main/java/simple_processing_App/FunctionManager.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class FunctionManager {
 
+    // funkcja dodaje nowego studenta do listy
     public static void addNewStudent(List<Student> list){
         Scanner input = new Scanner(System.in);
         System.out.printf("%s: ","Wprowadź imię");
@@ -18,10 +19,11 @@ public class FunctionManager {
         System.out.println("Poprawnie dodano studenta: " + student.getName());
     }
 
+    // funkcja wyszukuje studenta po imieniu, następnie można przypisać uczniowi wynik z egzaminu
     public static void assignExamGrade(List<Student> list){
 
-        final int MAX_VALUE = 100;
-        final int MIN_VALUE = 0;
+//        final int MAX_VALUE = 100;
+//        final int MIN_VALUE = 0;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Wpisz imię studenta ");
@@ -41,5 +43,19 @@ public class FunctionManager {
             }
         }
         System.out.println("Przypisano ocenę z egzaminu");
+    }
+
+    // funkcja wyszukuje i wyświetla studenta
+    public static void findStudentByName(List<Student> list){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Wyszukaj po imieniu: ");
+        while(!input.hasNext("\\D{3,15}+")){
+            input.next();
+            System.out.print("Niepoprawne imię. Wpisz poprawne imię: ");
+        }
+        String name = input.next();
+        list.stream()
+                .filter(st -> st.getName().equalsIgnoreCase(name))
+                .forEach(System.out::println);
     }
 }
