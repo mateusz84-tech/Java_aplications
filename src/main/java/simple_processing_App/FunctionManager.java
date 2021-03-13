a46d337079f0eed9422c5ec40982324a8ac0233f
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-13 19:07:53 +0100

    Created new function to assign grade to Student.

--- src/main/java/simple_processing_App/FunctionManager.java
+++ src/main/java/simple_processing_App/FunctionManager.java
@@ -17,4 +17,29 @@ public class FunctionManager {
         list.add(student);
         System.out.println("Poprawnie dodano studenta: " + student.getName());
     }
+
+    public static void assignExamGrade(List<Student> list){
+
+        final int MAX_VALUE = 100;
+        final int MIN_VALUE = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Wpisz imię studenta ");
+        while(!input.hasNext("\\D{3,15}+")){
+            input.next();
+            System.out.print("Niepoprawne dane. Wpisz imię: ");
+        }
+        String name = input.next();
+        for(Student student : list){
+            if(student.getName().equals(name)){
+                System.out.print("Przypisz ocenę: ");
+                while(!input.hasNextInt()) {
+                    input.nextInt();
+                }
+                int grade = input.nextInt();
+                student.setGrade(grade);
+            }
+        }
+        System.out.println("Przypisano ocenę z egzaminu");
+    }
 }
