ea0759c79cece1678f54e7062d71a81f042fb951
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-16 21:17:40 +0100

    Added a remove option.

--- src/main/java/simple_processing_App/DisplayMenu.java
+++ src/main/java/simple_processing_App/DisplayMenu.java
@@ -9,9 +9,10 @@ public final class DisplayMenu {
         System.out.printf("%s%n","3. Wyszukaj studenta.");
         System.out.printf("%s%n","4. Pokaż wszystkich studentów.");
         System.out.printf("%s%n","5. Pokaż średnią z egzaminu.");
-        System.out.printf("%s%n","6. Pokaż studenta z najwyższą oceną");
+        System.out.printf("%s%n","6. Pokaż studenta z najwyższą oceną.");
         System.out.printf("%s%n","7. Pokaż studenta z najniższą oceną.");
-        System.out.printf("%s%n","8. Graficzne przedstawienie wyników.");
-        System.out.printf("%s%n","9. Zakończ program.");
+        System.out.printf("%s%n","8. Usuń studenta.");
+        System.out.printf("%s%n","9. Graficzne przedstawienie wyników.");
+        System.out.printf("%s%n","10. Zakończ program.");
     }
 }
