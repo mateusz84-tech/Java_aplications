9a6ce3d71fb11a2ecee9bf3e5b276d9a2ddea505
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-21 19:30:23 +0100

    Check to simple code password. Add simple validation using regex check to correct text.

--- src/main/java/BCrypt/BCryptClass.java
+++ src/main/java/BCrypt/BCryptClass.java
@@ -11,6 +11,10 @@ public class BCryptClass {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
         Scanner input = new Scanner(System.in);
         System.out.print("Wpisz hasło: ");
+        while(!input.hasNext("\\D{8,12}+[1-9]+")){
+            input.next();
+            System.out.println("Hasło powinno zwierać litery i cyfry. Min 8 znaków max 12");
+        }
         String password = input.next();
         String result = encoder.encode(password);
         System.out.println("Wpisane hasło: " + password);
