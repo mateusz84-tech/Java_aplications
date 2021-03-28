7245379aeb33a810e29ff21c1e8edb1bf1172432
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-28 19:41:40 +0200

    Created bean ManagerMemoryStudent.

--- src/main/java/DiAppContext.java
+++ src/main/java/DiAppContext.java
@@ -1,3 +1,5 @@
+import beans.MangerMemoryStudent;
+import beans.Student;
 import config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,5 +11,14 @@ public class DiAppContext {
         //
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MangerMemoryStudent student =
+                context.getBean("memoryStudent",MangerMemoryStudent.class);
+        student.addStudent(new Student(1,"Ala","Nowak",80));
+        student.addStudent(new Student(2,"Jan","Kowalski",85));
+        student.addStudent(new Student(3,"Aga","Bąk",95));
+
+        student.displayAll();
+
     }
 }
