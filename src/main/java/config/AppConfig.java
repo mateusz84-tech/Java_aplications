191f03834f9b8dbc796228889b725d35631510fd
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-28 19:42:22 +0200

    Created bean ManagerMemoryStudent

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,7 +1,14 @@
 package config;
 
+import beans.MangerMemoryStudent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public MangerMemoryStudent memoryStudent(){
+        return new MangerMemoryStudent();
+    }
 }
