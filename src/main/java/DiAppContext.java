77fdd7cb0af8b8c7257a5f8f1ba3aacb5abc3c95
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-28 18:36:02 +0200

    Created AppConfig class to keep beans.

---
+++ src/main/java/DiAppContext.java
@@ -0,0 +1,12 @@
+import config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DiAppContext {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+}
