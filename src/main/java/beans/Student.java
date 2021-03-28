ad6dc6d42067b046cbecea7b8eec5b90adc5a28a
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-28 18:54:33 +0200

    Created class Student.

---
+++ src/main/java/beans/Student.java
@@ -0,0 +1,54 @@
+package beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private final int note;
+
+    public Student(int id, String firstName, String lastName, int note) {
+        if(id < 0 ){
+            throw new IllegalArgumentException("Id nie może być ujemne.");
+        }
+        if(firstName.isEmpty() || firstName.isBlank() || firstName == null){
+            throw new IllegalArgumentException("Błędne dane");
+        }
+        if(lastName.isEmpty() || lastName.isBlank() || lastName == null){
+            throw new IllegalArgumentException("Błędne dane");
+        }
+        if(note < 0 || note > 100){
+            throw new IllegalArgumentException("Błędne dane");
+        }
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.note = note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%n %s: %d%n %s %s%n %s: %d",
+                "Student","Id",getId(),getFirstName(),getLastName(),
+                "Ocena",getNote());
+    }
+}
