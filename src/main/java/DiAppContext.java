43a04422f1379a2636b178a231f091d16afdcab5
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-31 20:48:05 +0200

    Initial commit.

--- src/main/java/DiAppContext.java
+++ src/main/java/DiAppContext.java
@@ -1,6 +1,7 @@
 import beans.MangerMemoryStudent;
 import beans.Student;
 import config.AppConfig;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,13 +14,11 @@ public class DiAppContext {
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
         MangerMemoryStudent student =
-                context.getBean("memoryStudent",MangerMemoryStudent.class);
+                context.getBean( "memoryStudent",MangerMemoryStudent.class);
         student.addStudent(new Student(1,"Ala","Nowak",80));
-        student.addStudent(new Student(2,"Jan","Kowalski",85));
-        student.addStudent(new Student(3,"Aga","Bąk",95));
-
-        student.displayAll();
-        student.removeStudent(1);
+        student.addStudent(new Student(2,"Jan","Kowalski",10));
+        student.showListSize();
+        student.removeStudent(0);
         student.displayAll();
 
     }
