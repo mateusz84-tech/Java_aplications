331243272edf4215c91386bbd04048273106c09c
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-04-16 19:28:38 +0200

    initial commit.

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -2,10 +2,25 @@ package config;
 
 import beans.*;
 import org.springframework.context.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
-@ComponentScan(basePackages = "config")
-public class AppConfig {
+@ComponentScan(basePackages = "java")
+public class AppConfig{
 
     @Bean
     public LogService logService(){
