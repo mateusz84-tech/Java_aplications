8509a2c1b7343b7c7f0498e28ed471795088966f
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-04-16 20:12:51 +0200

    Create product class with method to calculate value and method to display all value.

---
+++ src/main/java/beans/shopApp/Product.java
@@ -0,0 +1,57 @@
+package beans.shopApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Product {
+
+    private final String productName;
+    private int quantity;
+    private double price;
+
+    public Product(String productName, int quantity, double price) {
+        if(productName.isBlank() || productName.isEmpty()){
+            throw new IllegalArgumentException("Nazwa nie może być pusta.");
+        }
+        if(quantity < 0){
+            throw new IllegalArgumentException("Ilość nie może być mniejsza od zera.");
+        }
+        if(price < 0.0){
+            throw new IllegalArgumentException("Cena nie może być mniejsza od zera.");
+        }
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    // metoda zwracająca wartość pozycji zamówienia
+    public double calculateValue(){
+        return getPrice() * getQuantity();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s %25.02f zł %5d szt. %10.02f zł.%n",
+                getProductName(),getPrice(), getQuantity(),calculateValue());
+    }
+}
