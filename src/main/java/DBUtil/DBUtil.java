bcd34b66daf1a8335cb41da2770dc85129f57f07
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-04-21 18:56:15 +0200

    Created more flexible function Connection by added parameter dbName and replace bd_name to this parameter.

--- src/main/java/DBUtil/DBUtil.java
+++ src/main/java/DBUtil/DBUtil.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
 
 public class DBUtil {
 
-    private static final String url = "jdbc:mysql://localhost:3306/db_mk?serverTimezone=UTC&useSSL=false&characterEncoding=utf8";
+    private static final String url = "jdbc:mysql://localhost:3306/db_name?serverTimezone=UTC&useSSL=false&characterEncoding=utf8";
     private static final String user = "root";
     public static void main(String[] args) throws SQLException {
 
-        System.out.println(conn());
+        System.out.println(conn("db_mk"));
     }
 
-    public static Connection conn() throws SQLException{
+    public static Connection conn(String dbName) throws SQLException{
         Scanner input = new Scanner(System.in);
         System.out.print("Podaj hasło: ");
         String password = input.next();
-        return DriverManager.getConnection(url,user,password);
+        return DriverManager.getConnection(url.replace("db_name",dbName),user,password);
     }
 
 }
